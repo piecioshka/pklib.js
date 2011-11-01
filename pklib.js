874d88c9aef26e2dcf3f7754e06d83a50410e990
Piotr Kowalski <piecioshka@gmail.com>
2011-11-01 16:10:35 +0100

    add new version

--- pklib.js
+++ pklib.js
@@ -1,19 +1,27 @@
 /**
- * pklib JavaScript library v1.0.0
+ * pklib JavaScript library v1.0.1
  * http://pklib.com/
  * 
- * Copyright 2011, Piotr Kowalski
- * Released under the GPL 3.0 Licenses.
- * http://pklib.com/license
+ * Copyright (c) 2011
  * 
- * Date: Mon Sep 26 13:27:16 GMT 2011 GMT+0200 (CEST)
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Date: Tue Nov 01 2011 16:05:01 GMT+0100 (CEST)
  */
 
 // pklib definition and initialization
 pklib = this.pklib || {
     author: "Piotr Kowalski",
-    www: "http://pklib.pl/",
-    version: "1.0.0",
+    www: "http://pklib.com/",
+    version: "1.0.1",
 };
 
 	
