c304c61315e6e6fcc96e0b05c985711ba0fcd634
Piotr Kowalski <piecioshka@gmail.com>
2011-11-05 19:04:21 +0100

    new bind

--- pklib.js
+++ pklib.js
@@ -1167,33 +1167,20 @@ pklib.profiler = (function () {
 /**
  * @package prototypes
  */
-Function.prototype.bind = Function.prototype.bind || function (oThis) {
-    if (typeof this !== "function") {
-        throw new TypeError("Function.prototype.bind - what is trying to be fBound is not callable");
-    }
-
-    var aArgs = Array.prototype.slice.call(arguments, 1), 
-        fToBind = this, 
-        fNOP = function () {
-            // pass
-        },
-        fBound = function () {
-            return fToBind.apply(this instanceof fNOP 
-                ? this 
-               : oThis || window, aArgs.concat(Array.prototype.slice.call(arguments)));
-        };
-
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-
-    return fBound;
-};
-
-Function.prototype.addMethod = function (name, func) {
+Function.prototype.method = function (name, func) {
     this.prototype[name] = func;
     return this;
 };
-	
+
+Function.method("bind", function (that) {
+    var method = this,
+        slice = Array.prototype.slice,
+        args = slice.apply(arguments, [1]);
+        
+    return function () {
+        return method.apply(that, args.concat(slice.apply(arguments, [0])))
+    }
+});	
 /**
  * @package string
  */
