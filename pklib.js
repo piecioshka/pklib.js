f6ac2b8b92a7a42eae76b707055c021fda567ec2
adamminiuk <adam.miniuk@gmail.com>
2013-09-16 20:00:18 +0200

    Fix string.capitalize()

--- pklib.js
+++ pklib.js
@@ -1469,7 +1469,7 @@ if (typeof Function.prototype.bind !== "function") {
      * @return {string}
      */
     function capitalize(source) {
-        return source.substr(0, 1).toUpperCase() + source.substring(1, source.length).toLowerCase();
+        return source.substr(0, 1).toUpperCase() + source.substring(1, source.length);
     }
 
     /**
